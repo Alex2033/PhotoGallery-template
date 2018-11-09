d1fee413749287c6abba0aae6fadc97caeceb8da
Александр <XoxlofAlexandr@yandex.ru>
2018-11-09 03:00:46 +0300

    1. Сверстан слайдер на главной странице 2. Сверстан сайдбар

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+$(function() {
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            slidesPerView: 1,
+            nextEl: '.swiper-arrow-next',
+            prevEl: '.swiper-arrow-prev',
+            
+        },
+        loop: true,
+        speed: 600
+      });
+});
